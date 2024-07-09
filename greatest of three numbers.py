0d3c47897e9552d8ca23f00eddce28893f02a382
muddalateja <160223683+muddalateja@users.noreply.github.com>
2024-07-09 10:37:38 +0530

    Create greatest of three numbers.py

---
+++ greatest of three numbers.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+c=int(input("Enter c value:"))
+if(a>b and a>c):
+    print("A is big")
+elif(b>a and b>c):
+    print("B is big")
+else:
+    print("C is big")
+
