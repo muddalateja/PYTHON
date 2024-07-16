21d389157ad0119c0736d8515afe85bf4025af9a
muddalateja <160223683+muddalateja@users.noreply.github.com>
2024-07-16 10:36:24 +0530

    Create triangle valid or not using if-else condition.py

---
+++ triangle valid or not using if-else condition.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number:"))
+if(a+b>c and a+c>b and b+c>a):
+	print("It form Vaild triangle")
+else:
+	print("It not form Vaild triangle")
