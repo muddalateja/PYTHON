510dacda42d2d2b6e2ed89bfc57a453ec1d02eb3
muddalateja <160223683+muddalateja@users.noreply.github.com>
2024-07-16 12:34:34 +0530

    Create Asssignment operators.py

---
+++ Asssignment operators.py
@@ -0,0 +1,17 @@
+s=int(input("Enter value of s:"))
+s+=3
+print("S=",s)
+s-=3
+print("S=",s)
+s*=3
+print("S=",s)
+s/=3
+print("S=",s)
+s%=3
+print("S=",s)
+s//=3
+print("S=",s)
+s**=3
+print("S=",s)
+s&=3
+print("S=",s)
