051a241b164c90ef3c52679c8c358f76d6601542
muddalateja <160223683+muddalateja@users.noreply.github.com>
2024-07-16 10:30:46 +0530

    Create greatest of three numbers using elif.py

---
+++ greatest of three numbers using elif.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number:"))
+if(a>b and a>c):
+	print("A is big")
+elif(b>a and b>c):
+	print("B is big")
+else:
+	print("C is big")
