d2a65bf6439e4e45a71d41ebbc86c06b7b9de624
muddalateja <160223683+muddalateja@users.noreply.github.com>
2024-07-16 11:20:18 +0530

    Create determine grade by marks elif.py

---
+++ determine grade by marks elif.py
@@ -0,0 +1,13 @@
+marks=int(input("Enter a number"))
+if(marks>=90):
+    print("A Grade")
+elif(marks>=80):
+	print("B Grade")
+elif(marks>=70):
+	print("C Grade")
+elif(marks>=60):
+	print("D Grade")
+elif(marks>=50):
+	print("E Grade")
+else:
+	print("Fail")
