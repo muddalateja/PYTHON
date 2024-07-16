9abe163bb2ac277ffcdd913671863ee1131c45a2
muddalateja <160223683+muddalateja@users.noreply.github.com>
2024-07-16 12:26:35 +0530

    Create identity operators.py

---
+++ identity operators.py
@@ -0,0 +1,11 @@
+t=int(input("Enter value of t:"))
+s=int(input("Enter value of s:"))
+v=t
+r=s
+print(t is v)
+print(t is not s)
+print (t is s)
+print(t is not v)
+print(s is r)
+print(s is not r)
+print (t is not r)
